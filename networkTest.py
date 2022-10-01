4af059aa78a989b1b4f99fd10776aad3cecfbac6
Giaffreda <43539325+Giaffreda@users.noreply.github.com>
2022-10-01 12:42:05 +0200

    Add files via upload

--- networkTest.py
+++ networkTest.py
@@ -1,18 +1,33 @@
 import snap
+import math
 import TSSAlgorithm as tss
 
 #run the algorithm with a static threshold
 def runStaticThreshold():
     graph = snap.LoadEdgeList(snap.TUNGraph, "Dataset/facebook_clean_data/politician_edges.csv", 0, 1, ',')
 
-    for threshold in range(1, 11):
+    for threshold in range(1, 16):
         hash_table = snap.TIntH()
 
         #Set the static threshold for each node in the hash table
         for node in graph.Nodes():
-            hash_table[node.GetId()] = threshold
+            hashTable[node.GetId()] = threshold
 
-        seedSet = tss.tssAlgorithm(snap.ConvertGraph(type(graph), graph), hash_table)
+        seedSet = tss.tssAlgorithm(snap.ConvertGraph(type(graph), graph), hashTable)
 
-        print("t = " + str(threshold) + " - seed set size = " + str(len(seedSet)))
+        print("threshold: " + str(threshold) + "; seed set size: " + str(len(seedSet)))
 
+def runProportionalThreshold():
+    # Load Graph
+    graph = snap.LoadEdgeList(snap.TUNGraph, "Dataset/facebook_clean_data/politician_edges.csv", 0, 1,',')
+
+    for i in range(16, 1, -1):      # From 10 to 2
+        # Set the threshold for each node based on its degree
+        hashTable = snap.TIntH()
+        proportion = 1/i
+
+        for node in graph.Nodes():
+            hashTable[node.GetId()] = math.ceil(proportion * node.GetOutDeg())
+
+        seedSet = tss.tssAlgorithm(snap.ConvertGraph(type(graph), graph), hashTable)
+        print("proportion: " + str(proportion) + "; seed set size: " + str(len(seedSet)))
